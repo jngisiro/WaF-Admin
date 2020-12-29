549d0251aec9d28ca8b9bd5208e30911c6ddaf16
Paul Ngisiro <jngisiro@gmail.com>
2020-12-29 18:44:29 +0300

    :sparkles: feat: added profile page

--- src/app/dashboard/profile/profile.component.ts
+++ src/app/dashboard/profile/profile.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onAddAdmin() {
+    this.router.navigate(['add-admin']);
   }
-
 }
